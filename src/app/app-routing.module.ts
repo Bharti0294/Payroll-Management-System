0cccc4ce8e9643bac00ce9568c744e0e3143766f
Bharti0294 <bharti.nehul07@gmail.com>
2021-08-17 14:40:36 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { EmployeesComponent } from './employees/employees.component';
+import { DeductionComponent } from './deduction/deduction.component';
+import { IncomeComponent } from './income/income.component';
+const routes: Routes = [
+  {
+    path: "employees",
+    component: EmployeesComponent,
+  },
+  {
+    path: "deduction",
+    component: DeductionComponent,
+  },
+  {
+    path: "income",
+    component: IncomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+
+import { FooterComponent } from './footer/footer.component';
+import { DeductionComponent } from './deduction/deduction.component';
+import { IncomeComponent } from './income/income.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    
+    FooterComponent,
+         DeductionComponent,
+         IncomeComponent,
+        
   ],
   imports: [
     BrowserModule,
